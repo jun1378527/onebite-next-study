c362baf4e0e45c71ad7f402aa665edfaf8c1fdb4
jun1378527 <81806934+jun1378527@users.noreply.github.com>
2024-11-27 08:16:09 +0900

    end

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,59 +1,88 @@
-import SearchableLayout from "@/components/searchable-layout";
-import style from "./index.module.css";
-import { ReactNode } from "react";
-import BookItem from "@/components/book-item";
-import { InferGetStaticPropsType } from "next";
-import fetchBooks from "@/lib/fetch-books";
-import fetchRandomBooks from "@/lib/fetch-random-books";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import style from "./[id].module.css";
+import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
-export const getStaticProps = async () => {
-  const [allBooks, recoBooks] = await Promise.all([
-    fetchBooks(),
-    fetchRandomBooks(),
-  ]);
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+    // false : 404 Notfound
+    // blocking : SSR 방식
+    // true : SRR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  const id = context.params!.id;
+  const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
-      allBooks,
-      recoBooks,
+      book,
     },
   };
 };
 
-export default function Home({
-  allBooks,
-  recoBooks,
+export default function Page({
+  book,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta
+            property="og:description"
+            content="한입 북스에 등록된 도서들을 만나보세요"
+          />
+        </Head>
+        <div>로딩중입니다</div>
+      </>
+    );
+  }
+  if (!book) return "문제가 발생했습니다 다시 시도하세요";
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } =
+    book;
+
   return (
     <>
       <Head>
-        <title>한입북스</title>
-        <meta property="og:image" content="/thumbnail.png" />
-        <meta property="og:title" content="한입북스" />
-        <meta
-          property="og:description"
-          content="한입 북스에 등록된 도서들을 만나보세요"
-        />
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
       </Head>
       <div className={style.container}>
-        <section>
-          <h3>지금 추천하는 도서</h3>
-          {recoBooks.map(book => (
-            <BookItem key={book.id} {...book} />
-          ))}
-        </section>
-        <section>
-          <h3>등록된 모든 도서</h3>
-          {allBooks.map(book => (
-            <BookItem key={book.id} {...book} />
-          ))}
-        </section>
+        <div
+          className={style.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.subTitle}>{subTitle}</div>
+        <div className={style.author}>
+          {author} | {publisher}
+        </div>
+        <div className={style.description}>{description}</div>
       </div>
     </>
   );
 }
-
-Home.getLayout = (page: ReactNode) => {
-  return <SearchableLayout>{page}</SearchableLayout>;
-};
